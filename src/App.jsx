a9aa1bc835d1b706b76e77fae8634ec5285d266e
Shagun Karmacharya <72041905+Shogun486@users.noreply.github.com>
2024-05-05 12:35:15 -0500

    search results showing

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,19 @@ import {useState, useEffect} from 'react'
 import ArtCard from './ArtCard';
 import './index.css'
 
-
-const API_URL = "https://api.artic.edu/api/v1/artworks?page=100&limit=100";
-let image = '';
-
 const App = () => {
-
+ 
   const [artworks, setArtworks] = useState([]);
   const [search, setSearch] = useState('');
 
   const searchArt = async (term) => {
-    const response = await fetch(`${term}`);
+    let response = await fetch(`https://api.artic.edu/api/v1/artworks/search?q=${term}`)
     const data = await response.json();
-    image = data.config.iiif_url;
     setArtworks(data.data);
-    console.log(data);
   }
 
   useEffect(() => {
-    searchArt(API_URL);
+    searchArt('cats');
   }, []);
 
   return (
@@ -34,7 +28,8 @@ const App = () => {
       <div>
         <div>
           <form className="relative flex items-centers justify-center">
-            <input type="text" id="search" placeholder="Hagrid . . ." className="border-0 focus:ring-0 focus:outline-0 w-[60%] bg-slate-500 rounded-l-lg pl-4 text-sm text-slate-200"/>
+            <input value={search}
+                   onChange={(e) => {setSearch(e.target.value); searchArt(search);}}  type="text" id="search" placeholder="Hagrid . . ." className="border-0 focus:ring-0 focus:outline-0 w-[60%] bg-slate-500 rounded-l-lg pl-4 text-sm text-slate-200"/>
             <button className="ring-4  ring-slate-900 ring-offset-[0.55rem] shadow-transparent ring-offset-red hover:ring-offset-white  hover:bg-white bg-white rounded-[50%] active:scale-95 cursor-pointer">
               <h2 className="rounded-full border-4 border-slate-800 w-16 h-16  text-red-900 text-2xl text-center justify-center flex items-center font-semibold hover:border-slate-600 hover:text-black ">
                 GO
@@ -45,22 +40,19 @@ const App = () => {
       </div>
       <br/>
 
-
       {/* Displaying Artwork */}
       {artworks.length > 0 ? (
         <div className = "container grid grid-flow-row-dense grid-cols-3 grid-rows-3">
           {artworks.map((artwork) => (
-           
-          <ArtCard card ={artwork} image={image}/>
+            <ArtCard card ={artwork}/>
           ))}
         </div>
         ):(
         <div className="empty">
-          <h2>No movies found</h2>
+          <h2>No artworks found</h2>
         </div>
         )
       }
-
     </div>
   )
 }
--- src/ArtCard.jsx
+++ src/ArtCard.jsx
@@ -1,22 +1,53 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 
-const ArtCard = ({card, image}) => {
-  return (
-    <div className="self-center relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
-      <div className="relative mx-4 mt-4  text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl h-70">
-        <img className="place-content-center object-contain" src={`${image}/${card.image_id}/full/843,/0/default.jpg`} />
-      </div>
-      <div className="p-6 text-center">
-        <h4 className="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-          {card.title}
-        </h4>
-        <p
-          className="block font-sans text-base antialiased font-medium leading-relaxed text-transparent bg-clip-text bg-gradient-to-tr from-blue-gray-600 to-blue-gray-400">
-          {card.artist_title}
-        </p>
-      </div>
+const ArtCard = ({card}) => {
+  let [artist, setArtist] = useState('');
+  let [artName, setArtName] = useState('');
+  let [pre_iiif_url, setPreURL] = useState('');
+  let [image_id_no, setImageID] = useState(0);
+  let [image_url, setImageURL] = useState('');
+
+  const searchImage = async (id) => {
+    let response = await fetch(`https://api.artic.edu/api/v1/artworks?ids=${id}&fields=image_id,artist_title,title`)
+    let keys = await response.json();    
+    let info = keys.data;
+    
+    artist = info[0].artist_title;
+    artName = info[0].title;
+    pre_iiif_url = keys.config.iiif_url;
+    image_id_no = info[0].image_id;
+    image_url = `${pre_iiif_url}/${image_id_no}/full/843,/0/default.jpg`;
+
+    setArtist(artist);
+    setArtName(artName);
+    setPreURL(pre_iiif_url);
+    setImageID(image_id_no);
+    setImageURL(image_url);
+    console.log(artist, artName, image_url);
+  }
+
+  useEffect(() => {
+    searchImage(card.id);
+  }, []);
+
+return (
+  <div className="self-center relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+    <div className="relative mx-4 mt-4  text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl h-70">
+      <img className="place-content-center object-contain" 
+           src={image_url !== 'null' ? image_url : 'https://via.placeholder.com/400'} />
     </div>
-  )
+    <div className="p-6 text-center">
+      <h4 className="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+        {artName !== null ? artName : 'Unknown Piece'}
+      </h4>
+      <p
+        className="block font-sans text-base antialiased font-medium leading-relaxed text-transparent bg-clip-text bg-gradient-to-tr from-blue-gray-600 to-blue-gray-400">
+        {artist !== 'null' ? artist : 'Unknown Artist'}
+      </p>
+    </div>
+  </div>
+)
 }
 
 export default ArtCard;
